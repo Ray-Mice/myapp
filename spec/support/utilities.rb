617793b5998fde437b915a485403a81937e0e1ee
Ray-Mice <princemice@126.com>
2013-09-23 13:25:13 +0800

    Finish layout, routes and add new controller User

---
+++ spec/support/utilities.rb
@@ -0,0 +1,11 @@
+include ApplicationHelper
+
+# original method to define full title without applicationhelper in /helper
+#def full_title(page_title)
+#  base_title = "Ruby on Rails 4.0.0 Tutorial"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end

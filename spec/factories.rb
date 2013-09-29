0f6269ea6cd03811121acf7fc2c8ca5f337b2b2e
Ray-Mice <princemice@126.com>
2013-09-29 10:23:12 +0800

    Complete User signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Issac Clark"
+    email    "beautyfreak89@gmail.com"
+    password "Loveyou123"
+    password_confirmation "Loveyou123"
+  end
+end
